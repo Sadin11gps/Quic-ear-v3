12f7d8b05cefefd4ba3ca947917f2a5e09bfdca0
Sadin11gps <amiratul8133@gmail.com>
2026-01-02 21:46:07 +0600

    Delete app directory

--- app/models/company.rb
+++
@@ -1,4 +0,0 @@
-class Company < ActiveRecord::Base
-  attr_accessible :address, :email, :name, :password,:password_confirmation,:organisation, :organisation_type, :password_digest
-  has_secure_password
-end
